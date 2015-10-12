fe926ab14a223c49ca013f984adbda2635f44d1f
sjeert <sjoerdteunisse@outlook.com>
2015-10-12 09:07:16 +0200

    Added forms 12-10-2015
    Added some forms.

--- BarrocITApp/BarrocITApp/Forms/EditProject.Designer.cs
+++ BarrocITApp/BarrocITApp/Forms/EditProject.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace BarrocITApp
+﻿namespace BarrocITApp.Forms
 {
-    partial class Form1
+    partial class EditProject
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "EditProject";
         }
 
         #endregion
     }
 }
-
