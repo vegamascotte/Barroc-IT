8a4563c3436b6b78cdf00638e5a57867a5ac39cc
sjeert <sjoerdteunisse@outlook.com>
2015-10-12 09:11:00 +0200

    Added extra forms

---
+++ BarrocITApp/BarrocITApp/Forms/EnrollQuatation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarrocITApp.Forms
+{
+    public partial class EnrollQuatation : Form
+    {
+        public EnrollQuatation()
+        {
+            InitializeComponent();
+        }
+    }
+}
