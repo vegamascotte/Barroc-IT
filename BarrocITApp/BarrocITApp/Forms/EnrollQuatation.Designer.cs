4e2a8b74dc173b8f776d6034d9db2ae2576f097c
sjeert <sjoerdteunisse@outlook.com>
2015-10-13 11:30:15 +0200

    Change resolution screens

--- BarrocITApp/BarrocITApp/Forms/EnrollQuatation.Designer.cs
+++ BarrocITApp/BarrocITApp/Forms/EnrollQuatation.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EnrollQuatation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 542);
+            this.Name = "EnrollQuatation";
             this.Text = "EnrollQuatation";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
