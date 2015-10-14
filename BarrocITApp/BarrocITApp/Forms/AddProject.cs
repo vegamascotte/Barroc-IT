95176ccd016f4b9d22fbaee6d9a72bc8dd7e889e
Jeebes <progagguitar@hotmail.com>
2015-10-14 10:50:37 +0200

    Add project form update 14-10-2015

--- BarrocITApp/BarrocITApp/Forms/AddProject.cs
+++ BarrocITApp/BarrocITApp/Forms/AddProject.cs
@@ -16,5 +16,10 @@ namespace BarrocITApp.Forms
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
